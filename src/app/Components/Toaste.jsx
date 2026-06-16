59f5a6734d95292bfcb453057bd5147ce58557a5
PBSsathi <pbs2k04@gmail.com>
2026-06-16 10:51:11 +0530

    button name changes

--- src/app/Components/Toaste.jsx
+++ src/app/Components/Toaste.jsx
@@ -11,7 +11,7 @@ export default function Toaste() {
   return (
     <>
       <button onClick={notify} className="bg-blue-500 w-50 border justify-center border-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Show Toast
+        Toast Click
       </button>
       <ToastContainer />
     </>
